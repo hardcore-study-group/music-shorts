00927dbb9a0a5e42c6ae01ab395a1957d6a96c48
hyun <coderhyun476@gmail.com>
2022-01-12 17:15:49 +0900

    Update eslint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,33 +5,29 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
+    '/lib/**/*', // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-    "prettier"
-  ],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "import/no-unresolved": 0,
-    "max-len": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "require-jsdoc": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'prettier/prettier': 'error',
+    'import/no-unresolved': 0,
+    'max-len': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'require-jsdoc': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
