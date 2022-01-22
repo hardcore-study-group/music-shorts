042cb63c8e35778bf803e241a866bdf446fd9359
hyun <coderhyun476@gmail.com>
2022-01-22 15:41:24 +0900

    Install firebase

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'music-shorts',
+    appId: '1:866427811004:web:fbf1e9b6ca413197e48cb9',
+    databaseURL: 'https://music-shorts-default-rtdb.firebaseio.com',
+    storageBucket: 'music-shorts.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCcuimm-R6FNygaQjH9rMSXRdG6srDRSvU',
+    authDomain: 'music-shorts.firebaseapp.com',
+    messagingSenderId: '866427811004',
+    measurementId: 'G-LBBRNVZ50H',
+  },
   production: false
 };
 
