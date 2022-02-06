d24d71d7c6707fa35c9e6ad8eef36123aae09a42
hyun <coderhyun476@gmail.com>
2022-02-06 17:10:23 +0900

    Install spotify api

--- app/src/screens/HomeScreen/index.tsx
+++ app/src/screens/HomeScreen/index.tsx
@@ -1,9 +1,29 @@
 import {Button, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import useNavigation from '../../hooks/useNavigation';
+import {ApiConfig, ApiScope, auth, remote} from 'react-native-spotify-remote';
+
+const spotifyConfig: ApiConfig = {
+  clientID: '',
+  redirectURL: '',
+  scopes: [ApiScope.AppRemoteControlScope, ApiScope.UserFollowReadScope],
+};
 
 const HomeScreen = () => {
   const {navigate} = useNavigation();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // const session = await auth.authorize(spotifyConfig);
+        await remote.connect('');
+        await remote.playUri('spotify:track:6IA8E2Q5ttcpbuahIejO74');
+        await remote.seek(58000);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
