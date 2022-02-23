e3fa850849119a4d2657849720397eedfaf13360
hyun <coderhyun476@gmail.com>
2022-02-23 18:48:21 +0900

    Implement auth api

--- functions/src/routes/auth/index.ts
+++ functions/src/routes/auth/index.ts
@@ -1,16 +1,27 @@
 import {Router} from 'express';
+import {spotify} from '../../config/spotify';
 
 const router = Router();
 
 router.get('/token/swap/:code', async (req, res) => {
   try {
-    console.log(req.params.code);
-    res.status(200).json();
+    const {code} = req.params;
+    const {body, statusCode} = await spotify.authorizationCodeGrant(code);
+    res.status(statusCode).json(body);
   } catch (error) {
     res.status(500).send(error);
   }
 });
 
-router.post('/token/refresh');
+router.post('/token/refresh', async (req, res) => {
+  try {
+    const {refresh_token} = req.body;
+    spotify.setRefreshToken(refresh_token);
+    const {body, statusCode} = await spotify.refreshAccessToken();
+    res.status(statusCode).json(body);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 export default router;
