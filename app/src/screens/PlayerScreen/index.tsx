cb3b2a92702c384fbc3e8daa169c13a15ac01b51
hyun <coderhyun476@gmail.com>
2022-03-06 14:57:17 +0900

    Implement play music

--- app/src/screens/PlayerScreen/index.tsx
+++ app/src/screens/PlayerScreen/index.tsx
@@ -6,7 +6,8 @@ import FastImage from 'react-native-fast-image';
 import {WIDTH} from '../../constants/styles';
 import {auth, remote, RepeatMode} from 'react-native-spotify-remote';
 import useNavigation from '../../hooks/useNavigation';
-import {spotifyConfig} from '../LoginScreen';
+import {SPOTIFY_CONFIG} from '../../constants/values';
+import axios from '../../config/axios';
 
 export interface PlayerScreenProps {
   index: number; // -1 meaning shuffle
@@ -20,16 +21,45 @@ const PlayerScreen = () => {
 
   useEffect(() => {
     (async () => {
-      await auth.endSession();
-      const session = await auth.authorize(spotifyConfig);
-      console.log(session);
-      await remote.connect(session.accessToken);
-      await remote.setShuffling(index === -1);
-      // await remote.setRepeatMode(RepeatMode.Off);
-      await remote.playUri('spotify:playlist:10anvRZxbRIG8DSJUW3fqW');
+      try {
+        // get playlist_id from my server
+        const {data} = await axios.get('/me');
+        const playlist_id = data.playlist_id;
+        // connect to spotify app
+        const isConnected = await remote.isConnectedAsync();
+        if (!isConnected) {
+          await auth.endSession(); // clear session
+          const session = await auth.authorize({
+            ...SPOTIFY_CONFIG,
+            playURI: `spotify:playlist:${playlist_id}`, // if no playURI at authorize, you can't play music
+          });
+          await remote.connect(session.accessToken); // connect
+        } else {
+          await remote.playUri(`spotify:playlist:${playlist_id}`); // when connected just setup to current playlist
+        }
+        // default option
+        await remote.setRepeatMode(RepeatMode.Context);
+        // options by index param
+        if (index !== -1) {
+          // when click playlist item
+          await remote.setShuffling(false);
+          const contentItem = await remote.getContentItemForUri(
+            `spotify:playlist:${playlist_id}`,
+          );
+          if (!contentItem) throw Error('no content item'); // android TODO
+          await remote.playItemWithIndex(contentItem, index); // play current playlist and seak to clicked item index
+        } else {
+          // when click shuffle
+          await remote.setShuffling(true);
+          await remote.skipToNext();
+        }
+      } catch (error) {
+        console.log(error);
+        goBack();
+      }
     })();
     return () => {
-      remote.pause().then(() => remote.disconnect());
+      remote.pause();
     };
   }, []);
 
