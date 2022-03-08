cd8503fa8a0a958dce00d62993b04e03d24af29a
hyun <coderhyun476@gmail.com>
2022-03-08 14:16:34 +0900

    Implement admin require & searhc api

--- functions/src/routes/search/index.ts
+++ functions/src/routes/search/index.ts
@@ -1,5 +1,18 @@
 import {Router} from 'express';
+import {spotify} from '../../config/spotify';
+import adminRequire from '../../middleware/adminRequire';
+import loginRequire from '../../middleware/loginRequire';
 
 const router = Router();
+
+router.get('/', loginRequire, adminRequire, async (req, res, next) => {
+  try {
+    const {q}: any = req.query;
+    const {body, statusCode} = await spotify.searchTracks(q, {limit: 15});
+    res.status(statusCode).json(body);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
--- functions/src/routes/tracks/index.ts
+++ functions/src/routes/tracks/index.ts
@@ -1,9 +1,35 @@
 import {Router} from 'express';
+import adminRequire from '../../middleware/adminRequire';
+import loginRequire from '../../middleware/loginRequire';
 
 import recommendation from './recomendation';
 
 const router = Router();
 
 router.use('/recomendation', recommendation);
+
+router.get('/', loginRequire, adminRequire, (req, res, next) => {
+  try {
+    console.log();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', loginRequire, adminRequire, (req, res, next) => {
+  try {
+    console.log();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', loginRequire, adminRequire, (req, res, next) => {
+  try {
+    console.log();
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
