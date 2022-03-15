55aa2fe02bacdc7d3bfd870a5f67f27117247790
hyun <coderhyun476@gmail.com>
2022-03-15 16:33:44 +0900

    Update ci logic

--- functions/src/config/errorLogger.ts
+++ functions/src/config/errorLogger.ts
@@ -1,7 +1,7 @@
 import {ErrorRequestHandler} from 'express';
 
 const errorLogger: ErrorRequestHandler = async (err, req, res, next) => {
-  if (process.env.NODE_ENV === 'test') console.error(err);
+  if (process.env.NODE_ENV === 'test') console.log(err);
   else require('firebase-functions/v1').logger.error(err);
   res.status(500).json(err);
 };
