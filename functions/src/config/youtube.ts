17effcc91d696cd10e788409ee75f586769e8197
hyun <coderhyun476@gmail.com>
2022-03-24 14:06:54 +0900

    Implement youtube search api

---
+++ functions/src/config/youtube.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import {config} from 'firebase-functions/v1';
+
+const youtube = axios.create({
+  baseURL: 'https://youtube.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    key: process.env.NODE_ENV === 'test' ? '' : config().youtube.key,
+  },
+});
+
+export default youtube;
